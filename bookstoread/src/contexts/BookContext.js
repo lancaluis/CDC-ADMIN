729667e435b6da23a18c5da02505fa25eb971fe3
luis Lança <luis.lanca@dte.local>
2020-01-07 10:02:56 -0300

    The Book List App has finished

--- bookstoread/src/contexts/BookContext.js
+++ bookstoread/src/contexts/BookContext.js
@@ -1,23 +1,18 @@
-import React, { createContext, useState } from "react";
-import uuid from "uuid/v1";
+import React, { createContext, useReducer, useEffect } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, setBooks] = useState([
-    { title: "Book 1", author: "Autor 1", id: 1 },
-    { title: "Book 2", author: "Autor 2", id: 2 }
-  ]);
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: uuid() }]);
-  };
-
-  const removeBook = id => {
-    setBooks([books.filter(book => book.id !== id)]);
-  };
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
